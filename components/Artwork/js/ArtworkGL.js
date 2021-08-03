01977a91bfc1b3807280795d669b4edc59ffadb9
mocchi1518a <m.ytr.1518@gmail.com>
2021-08-03 13:37:56 +0900

    test

---
+++ components/Artwork/js/ArtworkGL.js
@@ -0,0 +1,7 @@
+import * as THREE from "three";
+//　three.jsの処理を書いていく
+export default class ArtworkGL {
+  constructor(props) {
+    
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
+
 <template>
-  <p>testあああああああああああああああ</p>
+  <div>aaaaaaaaa</div>
 </template>
 
 <script>
